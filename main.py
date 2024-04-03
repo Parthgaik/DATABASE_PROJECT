9baca4a46447d93f1aa4ca5094184a78cef91a38
BURNSIDE HIGH SCHOOL <code@burnside.school.nz>
2024-04-04 09:26:14 +1300

    Made more functions for code

--- main.py
+++ main.py
@@ -1,59 +1,70 @@
 import sqlite3
 
-
+#Functions of code
 def show_all_cards():
     connection = sqlite3.connect("Yr13ClashRoyaleDB.db")
     cursor = connection.cursor()
-    cards = cursor.execute("SELECT * FROM Card;").fetchall()
-    print(cards)
-show_all_cards()
+    cards = cursor.execute("SELECT Name FROM Card;").fetchall()
+    for card in cards:
+        print(card[0])
+
 
 def cards_by_rarity():
     connection = sqlite3.connect("Yr13ClashRoyaleDB.db")
     cursor = connection.cursor()
-    rarity = input('please enter id of rarity')
+    rarity = input('please enter id of rarity ')
     rarity = int(rarity)
-    cardrarity = cursor.execute("SELECT * FROM Card WHERE Rarity = ?", (rarity,)).fetchall()
-    print(cardrarity)
+    cardrarity = cursor.execute("SELECT Name FROM Card WHERE Rarity = ?", (rarity,)).fetchall()
+    for card in cardrarity:
+        print(card[0])
 
-
-cards_by_rarity()
 
 def cards_by_elixir():
     connection = sqlite3.connect("Yr13ClashRoyaleDB.db")
     cursor = connection.cursor()
-    x = cursor.execute("SELECT * FROM Card ORDER BY Elixir;").fetchall()
-    print(x)
-functionname()
+    elixir = input('Please enter the elixir of card you want ')
+    elixir = int(elixir)
+    cardelixir = cursor.execute("SELECT Name, Elixir FROM Card ORDER BY Elixir;").fetchall()
+    for card in cardelixir:
+        print(card[0])
 
 
 
 
-def ():
+
+def all_troops():
     connection = sqlite3.connect("Yr13ClashRoyaleDB.db")
     cursor = connection.cursor()
-    x = cursor.execute("   ;").fetchall()
-    print(x)
-functionname()
+    troops = input('Please enter id of rarity ')
+    troops = int(troops)
+    alltroops = cursor.execute("SELECT Name FROM Card WHERE TypeID = ?", (troops,)).fetchall()
+    for card in alltroops:
+        print(card[0])
 
 
 
 
 
-def ():
-    connection = sqlite3.connect("Yr13ClashRoyaleDB.db")
-    cursor = connection.cursor()
-    x = cursor.execute("   ;").fetchall()
-    print(x)
-functionname()
+
+# def ():
+#     connection = sqlite3.connect("Yr13ClashRoyaleDB.db")
+#     cursor = connection.cursor()
+#     x = cursor.execute("   ;").fetchall()
+#     print(x)
+# functionname()
 
 
 
 
 
-def ():
-    connection = sqlite3.connect("Yr13ClashRoyaleDB.db")
-    cursor = connection.cursor()
-    x = cursor.execute("   ;").fetchall()
-    print(x)
-functionname()
+# def ():
+#     connection = sqlite3.connect("Yr13ClashRoyaleDB.db")
+#     cursor = connection.cursor()
+#     x = cursor.execute("   ;").fetchall()
+#     print(x)
+# functionname()
+
+
+if __name__ == "__main__":
+    #main code starts here
+    all_troops()
